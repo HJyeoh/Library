5e05770c3dfefc4fbb6130cae561936bb0518ab5
HJyeoh <147971774+HJyeoh@users.noreply.github.com>
2024-11-22 14:02:40 +0800

    Delete library try 2 directory

--- library try 2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.codewithmosh.librarytry2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.codewithmosh.librarytry2 to javafx.fxml;
-    exports com.codewithmosh.librarytry2;
-}
